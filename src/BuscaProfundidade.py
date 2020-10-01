4f72642ed7eef356524051862111508efe5c1b63
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-01 20:54:00 -0300

    feat(BuscarProfundidade.py): python pattern to function name

--- src/BuscaProfundidade.py
+++ src/BuscaProfundidade.py
@@ -10,7 +10,7 @@ grafo = [
 ]
 
 
-def verticesAdj(grafo, vertice):
+def selecionar_vertices_adjacentes(grafo, vertice):
     # Funcao para adicionar os vertices que estao por vir (pilha)
     adj = []
     for i in range(1, len(grafo[vertice])):
@@ -18,19 +18,19 @@ def verticesAdj(grafo, vertice):
     return adj
 
 
-def buscaProfundidade(grafo, vertice, visitados):
+def buscar_em_profundidade(grafo, vertice, visitados):
     # Funcao para realizar a busca em profundidade percorrendo os vertices
     if vertice in visitados:
         return False
     visitados.append(vertice)
-    for vizinho in verticesAdj(grafo, vertice):
+    for vizinho in selecionar_vertices_adjacentes(grafo, vertice):
         if vizinho not in visitados:
-            buscaProfundidade(grafo, vizinho, visitados)
+            buscar_em_profundidade(grafo, vizinho, visitados)
 
 
 # Pedir o vertice para inciar a busca
 visitados = []
 vertice = input("Informe o vertice inicial: ")
-buscaProfundidade(grafo, int(vertice), visitados)
+buscar_em_profundidade(grafo, int(vertice), visitados)
 
 print('visitados => ', visitados)
