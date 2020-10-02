1b570f4dc368e0b03182f061e48be3b90a45f639
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-01 22:03:52 -0300

    refactor: removing inputs

--- src/busca_largura.py
+++ src/busca_largura.py
@@ -1,17 +1,5 @@
 import src.utils as utils
 
-# Estrutura do grafo ja pronta
-grafo = [
-    [0, 1, 2],
-    [1, 0, 3, 4],
-    [2, 0, 5, 6],
-    [3, 1],
-    [4, 1],
-    [5, 2],
-    [6, 2]
-]
-
-
 def buscar_em_largura(grafo: list, vertice: int) -> list:
     # Funcao para realizar a busca em largura - buscados os vizinhos
     #   -> faz uma lista de vertices ja visitados
@@ -31,12 +19,3 @@ def buscar_em_largura(grafo: list, vertice: int) -> list:
                 visitados.append(vizinho)
 
     return visitados
-
-
-# Pedir o vertice para iniciar a busca
-vertice = input(
-    "Informe o vertice inicial (entre {} e {}): ".format(
-        grafo[0][0], grafo[-1][0])
-)
-vertices_visitados = buscar_em_largura(grafo, int(vertice))
-print('visitados =>', vertices_visitados)
--- src/busca_profundidade.py
+++ src/busca_profundidade.py
@@ -1,17 +1,5 @@
 import src.utils as utils
 
-# Estrutura do grafo ja pronta
-grafo = [
-    [0, 1, 2],
-    [1, 0, 3, 4],
-    [2, 0, 5, 6],
-    [3, 1],
-    [4, 1],
-    [5, 2],
-    [6, 2]
-]
-
-
 def buscar_em_profundidade(grafo: list, vertice: int, visitados: list) -> bool:
     # Funcao para realizar a busca em profundidade percorrendo os vertices
     if vertice in visitados:
@@ -20,14 +8,3 @@ def buscar_em_profundidade(grafo: list, vertice: int, visitados: list) -> bool:
     for vizinho in utils.selecionar_vertices_adjacentes(grafo, vertice):
         if vizinho not in visitados:
             buscar_em_profundidade(grafo, vizinho, visitados)
-
-
-# Pedir o vertice para inciar a busca
-visitados = []
-vertice = input(
-    "Informe o vertice inicial (entre {} e {}): ".format(
-        grafo[0][0], grafo[-1][0])
-)
-buscar_em_profundidade(grafo, int(vertice), visitados)
-
-print('visitados => ', visitados)
