da020f2d9f3b95d1206fa7515e10fffc11351ad5
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-01 21:21:44 -0300

    test: structure to tests

---
+++ tests/test_busca_largura.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestBuscaLargura(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_busca_profundidade.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestBuscaProfundidade(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
