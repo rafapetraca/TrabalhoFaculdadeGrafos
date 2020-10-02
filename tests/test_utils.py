b57a18f0765e4545514b04dadda77b3595921f66
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-01 21:29:43 -0300

    test(test_utils.py): if expected is equal to result

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,23 @@
 import unittest
+import src.utils as utils
+
 
 class TestUtils(unittest.TestCase):
+
+    def test_image_type(self):
+        grafo = [
+            [0, 1, 2],
+            [1, 0, 3, 4],
+            [2, 0, 5, 6],
+            [3, 1],
+            [4, 1],
+            [5, 2],
+            [6, 2]
+        ]
+        vertice = 2
+        adjacentes_esperados = [0, 5, 6]
+        adjacentes = utils.selecionar_vertices_adjacentes(grafo, vertice)
+        self.assertEqual(adjacentes, adjacentes_esperados)
 
 
 if __name__ == '__main__':
